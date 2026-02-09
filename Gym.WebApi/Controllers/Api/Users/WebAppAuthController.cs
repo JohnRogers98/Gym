8d5d57d11370b99b9153b943df24865b9e3bacdc
ivansystem <ShiniShini4257@gmail.com>
2026-02-09 15:44:05 +0300

    microfix

--- Gym.WebApi/Controllers/Api/Users/WebAppAuthController.cs
+++ Gym.WebApi/Controllers/Api/Users/WebAppAuthController.cs
@@ -18,6 +18,7 @@ namespace Gym.WebApi.Controllers.Api.Users
         [HttpPost]
         public async Task<ActionResult<WebAppAuthResponse>> WebAppAuth(WebAppAuthRequest request)
         {
+            Console.WriteLine("Должна прийти строка - " + request.InitData);
             UserDetails userDetails = await _mediator.Send(new AuthenticateUserCommand(request.InitData));
 
             String accessToken = _accessTokenGenerator.Generate(userDetails);
