8dc79231a45511784e19637a1ec999b01453643f
randolph <striker2015zhuzhma@gmail.com>
2026-03-05 14:31:09 +0300

    Api  endpoint features added: ListClients for admin and GetClient.

---
+++ Gym.WebApi/Controllers/Api/Clients/_DtoMappings.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using Gym.Abstractions.Query.Clients;
+using Gym.WebDto.Responses.Clients;
+
+namespace Gym.WebApi.Controllers.Api.Clients
+{
+    public class _DtoMappings : Profile
+    {
+        public _DtoMappings()
+        {
+            CreateMap<ClientProjection, ClientDto>();
+            CreateMap<ClientProjection, GetClientResponse>();
+        }
+    }
+}
